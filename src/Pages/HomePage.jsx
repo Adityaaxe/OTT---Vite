f75268d892da53701d8dd703fe028969c39fedf8
Adityaaxe <aditya.singh2021@sait.ac.in>
2025-02-16 01:35:01 +0530

    Card Update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <div className="bg-[#0F0F0F] min-h-screen text-white">
       <Carousel />
-      <div className="bg-black text-center text-white pt-5 text-lg">Watch Movies, Series, Drama, and much more... of different Countries</div>
+      <div className="bg-black text-center text-white pt-5 text-lg">Watch Movies, Series, Drama, and much more.. of different Countries</div>
       <CountryCardScroller />
     </div>
   );
